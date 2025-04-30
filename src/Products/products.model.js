51ac0060d9594dcdebfb301eda1a3e177fcd4f6f
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-04-30 00:10:24 -0600

    Update products.model.js

---
+++ src/Products/products.model.js
@@ -0,0 +1,55 @@
+import mongoose, {Schema, model} from "mongoose";
+
+
+const ProductsSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true,'Product name is required'],
+        trim: true,
+        required: [true,'Product name is required']
+    },
+    category:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    amount:{
+        type: Number,
+        required: true,
+        min:0,
+        default:0
+    },
+    description:{
+        type: String,
+        trim: true,
+    },
+    location:{
+        type: String,
+        trim: true
+    },
+    provider:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Provider',
+        required: true
+    },
+    removed:{
+        type: Boolean,
+        default: false
+    },
+    reasonDeleted:{
+        type: String,
+        default: null,
+    },
+    deleteFrom: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+    },
+    deletionDate:{
+        type: Date,
+        default: null
+    },
+    },{
+        timestamps: true
+})
+export default model('Products', ProductsSchema)
