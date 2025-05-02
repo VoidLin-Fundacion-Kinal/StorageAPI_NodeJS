d4e336d7fa90e3ed05fae1446b92142ae316d920
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-01 21:26:20 -0600

    Update app.js

--- configs/app.js
+++ configs/app.js
@@ -10,6 +10,7 @@ import cors from 'cors'
 import authRoutes from '../src/Auth/auth.routes.js'
 import userRoutes from '../src/User/user.routes.js'
 import providerRoutes from '../src/Provider/provider.routes.js'
+import productsRoutes from '../src/Products/products.routes.js'
 
 const configs = (app) => {
     app.use(express.json())
@@ -27,6 +28,7 @@ const routes = (app) => {
     app.use(authRoutes)
     app.use('/v1/user', userRoutes)
     app.use('/v1/provider', providerRoutes)
+    app.use('/v1/products', productsRoutes)
 
 }
 
