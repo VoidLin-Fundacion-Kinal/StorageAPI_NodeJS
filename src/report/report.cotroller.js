5382b7da664d91dbc42eb5f8f422a10b2719dafb
pconsuegra-2023018 <pconsuegra-2023018@kinal.edu.gt>
2025-05-04 15:49:06 -0600

    Create report.cotroller.js

---
+++ src/report/report.cotroller.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import productsModel from "../Products/products.model.js";
+//import { Collection } from "mongoose";
+
+
+ export const inventoryReport =async (req,res) => {
+    try {
+        const {limit,skip}=req.query
+        const products = await productsModel.find().select('_id name category amount price description')
+        .skip(skip)
+        .limit(limit)
+        let totalStock = 0
+        let inventory = 0
+    
+        products.forEach(product => {
+            totalStock += product.amount
+            inventory += product.amount * product.price
+        })
+        return res.send({
+            success: true,
+            message: "Product's report:",
+            products,
+            totalStockMessage: `Products in stock: ${totalStock}`,
+            inventoryMessage: `Inventory's value: ${inventory}`
+          });
+          
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({
+            success: false,
+            message: 'General error with invetoryReport',
+            error
+        })
+    }
+
+} 
