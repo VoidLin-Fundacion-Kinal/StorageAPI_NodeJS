9b32ff5a30862482fcdb900f83f84d98deef55f5
pconsuegra-2023018 <pconsuegra-2023018@kinal.edu.gt>
2025-05-04 21:38:47 -0600

    Update provider.routes.js

--- src/Provider/provider.routes.js
+++ src/Provider/provider.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express"
 import { deleteProvider, getAllProviders, saveProvider } from "./provider.controller.js"
 import { deleteProviderValidator, providerValidator } from "../../middlewares/validators.js"
-import { validateJwt} from '../../middlewares/validate.jwt.js'
+import { validateJwt, isAdmin} from '../../middlewares/validate.jwt.js'
 
 const api = Router()
 
@@ -12,7 +12,7 @@ api.post('/saveProvider',
         )
 
 api.delete('/deleteProvider/:id',
-            validateJwt,
+            [validateJwt,isAdmin],
             deleteProviderValidator,
             deleteProvider
 )
