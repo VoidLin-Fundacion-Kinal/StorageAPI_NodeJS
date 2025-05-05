8714bce6a8792eeaa4d23f9e3d82935957727cd5
dmarroquin-2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-05-04 20:40:49 -0600

    Update provider.routes.js

--- src/Provider/provider.routes.js
+++ src/Provider/provider.routes.js
@@ -1,18 +1,21 @@
 import { Router } from "express"
 import { deleteProvider, getAllProviders, saveProvider } from "./provider.controller.js"
 import { deleteProviderValidator, providerValidator } from "../../middlewares/validators.js"
+import { validateJwt} from '../../middlewares/validate.jwt.js'
 
 const api = Router()
 
 api.post('/saveProvider',
+            validateJwt,
             providerValidator,
             saveProvider
         )
 
 api.delete('/deleteProvider/:id',
+            validateJwt,
             deleteProviderValidator,
             deleteProvider
 )
 
-api.get('/getAllProvider', getAllProviders)
+api.get('/getAllProvider', validateJwt,getAllProviders)
 export default api
