06515a1aa17047319d33d790c06699aff4ecde2f
dmarroquin-2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-05-04 20:40:06 -0600

    Create products.model.js

---
+++ src/Products/products.model.js
@@ -0,0 +1,61 @@
+import {model, Schema} from "mongoose";
+
+const ProductsSchema = Schema({
+    name:{
+        type: String,
+        required: [true,'Product name is required'],
+        trim: true,
+        unique: [true, 'Product is already taken'], 
+    },
+    category:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    amount:{
+        type: Number,
+        required: true,
+        min:0,
+        default:0
+    },
+    description:{
+        type: String,
+        trim: true,
+    },
+    location:{
+        type: String,
+        trim: true
+    },
+    price:{
+        type: Number,
+        required: true,
+        default: 0,
+    },
+
+    provider:{
+        type: Schema.Types.ObjectId,
+        ref: 'Provider',
+        required: true
+    },
+    removed:{
+        type: Boolean,
+        default: false
+    },
+    reasonDeleted:{
+        type: String,
+        default: null,
+    },
+    deleteFrom: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+    },
+    deletionDate:{
+        type: Date,
+        default: null
+    },
+    },{
+        timestamps: true
+})
+
+export default model('Products', ProductsSchema)
