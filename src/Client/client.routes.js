4ce8179b66f5f60c8805eb156567bad8bea046ce
pconsuegra-2023018 <pconsuegra-2023018@kinal.edu.gt>
2025-05-04 21:38:35 -0600

    Update client.routes.js

--- src/Client/client.routes.js
+++ src/Client/client.routes.js
@@ -3,7 +3,7 @@ import { getAllClient,
     addClient,
     updateClient,
     deleteClient } from "./client.controller.js";
-import { validateJwt } from "../../middlewares/validate.jwt.js";
+import { validateJwt, isAdmin } from "../../middlewares/validate.jwt.js";
 import { clientValidator,clientValidatorId } from "../../middlewares/validators.js";
 
 const api = Router()
@@ -11,6 +11,6 @@ const api = Router()
 api.get('/getClient',validateJwt,getAllClient)
 api.post('/addClient',validateJwt,clientValidator,addClient)
 api.put('/updateClient/:id',validateJwt,clientValidator,clientValidatorId,updateClient)
-api.delete('/deleteClient/:id',validateJwt,clientValidatorId,deleteClient)
+api.delete('/deleteClient/:id',[validateJwt, isAdmin],clientValidatorId,deleteClient)
 
 export default api
