8c7ad163767ca0965d0acb434d82806d552d3faf
dmarroquin-2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-05-04 20:39:40 -0600

    Create client.routes.js

---
+++ src/Client/client.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { getAllClient,
+    addClient,
+    updateClient,
+    deleteClient } from "./client.controller.js";
+import { validateJwt } from "../../middlewares/validate.jwt.js";
+import { clientValidator,clientValidatorId } from "../../middlewares/validators.js";
+
+const api = Router()
+
+api.get('/getClient',validateJwt,getAllClient)
+api.post('/addClient',validateJwt,clientValidator,addClient)
+api.put('/updateClient/:id',validateJwt,clientValidator,clientValidatorId,updateClient)
+api.delete('/deleteClient/:id',validateJwt,clientValidatorId,deleteClient)
+
+export default api
