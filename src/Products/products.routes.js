74acce73a161e95f73061ec81918126a7b2f34f0
dmarroquin-2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-05-04 20:40:09 -0600

    Create products.routes.js

---
+++ src/Products/products.routes.js
@@ -0,0 +1,34 @@
+import { Router } from 'express'
+import { 
+        createProducts, 
+        getAllProducst, 
+        getProductById,
+        getProductByCategory,
+        getProductByDateCreate,
+        getProductByName,
+        updateProducts,
+        deleteProduct
+    } from './product.controller.js'
+import {
+        validateJwt
+    } from '../../middlewares/validate.jwt.js'
+import { 
+        createProductValidator, 
+        updateProductValidator,
+        deleteProductValidator, 
+        getProductByID
+    } from "../../middlewares/validators.js"
+  
+
+const api = Router()
+
+api.post('/products', validateJwt,  createProductValidator, createProducts)
+api.get('/products', validateJwt,   getAllProducst)
+api.get('/products/:id',validateJwt, getProductByID,  getProductById)
+api.get('/category/:category',validateJwt,  getProductByCategory)
+api.get('/name/:name',validateJwt,   getProductByName)
+api.get('/date/:createdAt',validateJwt, getProductByDateCreate)
+api.put('/products/:id',validateJwt,   getProductByID, updateProductValidator,  updateProducts)
+api.put('/product/:id',validateJwt,   getProductByID,  deleteProductValidator, deleteProduct)
+
+export default api
